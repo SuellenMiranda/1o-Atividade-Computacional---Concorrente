0b49f22aa30914b9d403cc653b59064e3677a4d7
Suh <suellen.org@gmail.com>
2023-03-20 08:16:39 -0300

    ContaBancaria.php

---
+++ Questão 1/ContaBancaria.php
@@ -0,0 +1,16 @@
+ContaBancaria
+------------------------
+- numeroConta: int
+- titularConta: String
+- saldo: double
+------------------------
++ ContaBancaria(numeroConta: int, titularConta: String, saldoInicial: double)
++ getNumeroConta(): int
++ setNumeroConta(numeroConta: int): void
++ getTitularConta(): String
++ setTitularConta(titularConta: String): void
++ getSaldo(): double
++ deposito(valor: double): void
++ saque(valor: double): boolean
++ toString(): String
+------------------------
